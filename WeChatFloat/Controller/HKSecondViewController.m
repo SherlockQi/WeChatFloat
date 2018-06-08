b226cd85118e5cac6cc2a1d1479c1d67cad4d2a7
SherlockQi <xiaqi90@163.com>
2018-06-08 11:28:58 +0800

    update

--- WeChatFloat/Controller/HKSecondViewController.m
+++ WeChatFloat/Controller/HKSecondViewController.m
@@ -20,7 +20,6 @@
     _imageView = [[UIImageView alloc]initWithFrame:self.view.bounds];
     [self.view addSubview:_imageView];
     _imageView.image = self.image;
-    self.navigationController.interactivePopGestureRecognizer.delegate = self;
 }
 
 
