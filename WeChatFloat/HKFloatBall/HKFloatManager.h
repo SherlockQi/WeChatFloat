6c851f55d59f3bce904e9cf1d500407add938722
SherlockQi <xiaqi90@163.com>
2018-06-11 15:15:59 +0800

    update

--- WeChatFloat/HKFloatBall/HKFloatManager.h
+++ WeChatFloat/HKFloatBall/HKFloatManager.h
@@ -14,4 +14,17 @@
 @property (nonatomic, strong) HKFloatBall *floatBall;
 + (instancetype)shared;
 + (void)addFloatVcs:(NSArray<NSString *>*)vcClass;
+
+/*
+如其它牛逼第三方设置了 interactivePopGestureRecognizer
+ 
+可以在interactivePopGestureRecognizer代理中调用该方法 并删除HKFloatManager.m 中的第45行
+ 
+如FDFullscreenPopGesture
+可以在 UINavigationController+FDFullscreenPopGesture.m 的
+- (BOOL)gestureRecognizerShouldBegin:(UIPanGestureRecognizer *)gestureRecognizer 返回 YES 前
+调用 [[HKFloatManager shared] beginScreenEdgePanBack:gestureRecognizer];
+并删除本项目 HKFloatManager.m 第45行
+ **/
+- (void)beginScreenEdgePanBack:(UIGestureRecognizer *)gestureRecognizer;
 @end
